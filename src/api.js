10fb1e70606c266e072033df9e55ee31dbff7ccd
Fakhruddin <fbhabrana@gmail.com>
2024-04-12 22:02:03 +0530

    Latest Commit 5

--- src/api.js
+++ src/api.js
@@ -1,48 +1,43 @@
 import axios from 'axios';
 
+// Define base URL for API endpoints
 const baseURL = process.env.BASE_URL || 'https://resize-backend-latest.vercel.app';
 
+// Create an axios instance with the base URL
 const axiosInstance = axios.create({
   baseURL,
 });
 
-// Function to add new data
-export const addData = async (formData) => {
+// Generic function to make API requests
+const apiRequest = async (method, url, data = null) => {
   try {
-    const response = await axiosInstance.post('api/add', formData);
+    const startTime = new Date(); // Record the start time
+    const response = await axiosInstance[method](url, data);
+    const endTime = new Date(); // Record the end time
+    const executionTime = endTime - startTime; // Calculate the execution time in milliseconds
+    console.log(`API ${method.toUpperCase()} ${url} took ${executionTime} ms to execute`);
     return response.data;
   } catch (error) {
     throw error;
   }
+};
+
+// Function to add new data
+export const addData = async (formData) => {
+  return apiRequest('post', 'api/add', formData); // Add data API
 };
 
 // Function to update existing data
 export const updateData = async (id, formData) => {
-  try {
-    const response = await axiosInstance.put(`api/update/${id}`, formData);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  return apiRequest('put', `api/update/${id}`, formData); // Update data API
 };
 
 // Function to get all data
 export const getAllData = async () => {
-  try {
-    const response = await axiosInstance.get('api/getData');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  return apiRequest('get', 'api/getData'); // Get all data API
 };
 
 // Function to get counts
 export const getCounts = async () => {
-  try {
-    const response = await axiosInstance.get('api/count');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching counts:', error);
-    throw error;
-  }
+  return apiRequest('get', 'api/count'); // Get counts API
 };
